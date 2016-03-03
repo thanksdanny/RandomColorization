07c04bfb0e266dfe54c3e561214fe4608766fdcb
thanksdanny <hj-07@163.com>
2016-03-03 14:55:26 +0800

    Initial Commit

---
+++ RandomColorization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomColorization
+//
+//  Created by Danny Ho on 3/3/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
