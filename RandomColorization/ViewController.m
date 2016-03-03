5459c137043e8b951eadf4ba920150223880ee52
thanksdanny <hj-07@163.com>
2016-03-03 16:21:51 +0800

    实现颜色变换

--- RandomColorization/ViewController.m
+++ RandomColorization/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
@@ -14,6 +15,40 @@
 
 @implementation ViewController
 
+- (void)randomColor {
+    NSLog(@"%s", __func__);
+    CGFloat redValue = drand48();
+    CGFloat blueValue = drand48();
+    CGFloat greenValue = drand48();
+    
+    self.view.backgroundColor = [UIColor colorWithRed:redValue green:greenValue blue:blueValue alpha:1.0];
+}
+
+#pragma mark - action
+- (IBAction)playMusciButtonDidTouch {
+    // mp3
+    
+    
+    // gradient
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(randomColor) userInfo:nil repeats:true];
+    
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    gradientLayer.frame = self.view.bounds;
+    
+    gradientLayer.colors = @[(__bridge id)[UIColor colorWithWhite:0.5 alpha:0.2].CGColor,
+                          (__bridge id)[UIColor colorWithRed:1.0 green:0 blue:0 alpha:0.4].CGColor,
+                          (__bridge id)[UIColor colorWithRed:0 green:1 blue:0 alpha:0.3].CGColor,
+                          (__bridge id)[UIColor colorWithRed:0 green:0 blue:1 alpha:0.3].CGColor,
+                          (__bridge id)[UIColor colorWithWhite:0.4 alpha:0.2].CGColor];
+    gradientLayer.locations = @[@(0.10), @(0.30), @(0.50), @(0.70), @(0.90)]; // 颜色分割线
+    gradientLayer.startPoint = CGPointMake(0, 0); // 起始点
+    gradientLayer.endPoint = CGPointMake(1, 1); // 结束点
+    [self.view.layer addSublayer:gradientLayer];
+    
+}
+
+
+#pragma mark - view
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
